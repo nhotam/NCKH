1f29014c1ba2641e5279b8981dc780f309a941ef
G75-tech <truongex147@gmail.com>
2025-07-28 15:37:04 +0700

    Update build_db.py

--- Chatbot_nckh/RAG/build_db.py
+++ Chatbot_nckh/RAG/build_db.py
@@ -41,3 +41,4 @@ vectorstore = Chroma.from_documents(all_docs, embedding=embedding, persist_direc
 vectorstore.persist()
 
 print(f"✅ Vector DB đã được tạo và lưu vào {VECTORSTORE_DIR}/")
+#đây là comment
